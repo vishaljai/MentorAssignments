e8827a02e0653f15a5e478b9bdc68236fb2e3945
Vishal Jain <vishaljain93@hotmail.com>
2016-05-25 12:09:44 +0530

    Second

--- Strings/src/cyb/assignment1/test1/StringTest.java
+++ Strings/src/cyb/assignment1/test1/StringTest.java
@@ -1,34 +1,51 @@
 package cyb.assignment1.test1;
 
-// Class to test String Pool
+/**
+ * @author Vishal Jain
+ * 
+ *         Class to test String Pool
+ * 
+ */
 public class StringTest {
 
 	// Global variable to be accessible everywhere
 	public static String name = "Vishal"; // Object in String Literal Pool
-	
+
 	public static void main(String[] args) {
-		
+
 		// Local variables
 		String name_again = new String("Vishal"); // Object create in heap
 		String name_test = "Vishal"; // Object in String Literal Pool
 		String name_test_again = "vishal"; // Object in String Literal Pool
-		
+
 		// Performing tests to check about String Literal Pool
-		System.out.println(name==name_again);
-		System.out.println(name==name_test);
-		System.out.println(name==name_test_again);
-		System.out.println(name_again==name_test_again);
-		System.out.println(name_test==name_test_again);
+		System.out.println("String Literal Pool Test: ");
+		System.out.println(name == name_again);
+		System.out.println(name == name_test);
+		System.out.println(name == name_test_again);
+		System.out.println(name_again == name_test_again);
+		System.out.println(name_test == name_test_again);
+
+		System.out.println("hashCode Test: ");
+
+		System.out.println(name.hashCode());
+		System.out.println(name_again.hashCode());
+		System.out.println(name_test.hashCode());
+		System.out.println(name_test_again.hashCode());
 
 	}
 }
-/* Output:
- * false
- * true
- * false
- * false
+/*
+ * Output: 
+ * String Literal Pool Test: 
  * false 
- */	
- 
-
-
+ * true 
+ * false 
+ * false 
+ * false 
+ * hashCode Test:
+ * -1732361581 
+ * -1732361581 
+ * -1732361581 
+ * -816228749
+ */
--- Strings/src/cyb/assignment1/test2/StringTest.java
+++ Strings/src/cyb/assignment1/test2/StringTest.java
@@ -1,20 +1,36 @@
 package cyb.assignment1.test2;
- 
-// Another class in other package to test String Pool
+
+/**
+ * @author Vishal Jain
+ * 
+ *         Another class in other package to test String Pool
+ * 
+ */
 public class StringTest {
-	
+
 	public static void main(String[] args) {
-	
+
 		// Local variable
 		String name1 = "Vishal";
 
 		// Importing global variable of another package, another class
-		System.out.println(cyb.assignment1.test1.StringTest.name==name1);// Comparing and getting the result
+		System.out.println(cyb.assignment1.test1.StringTest.name == name1);// Comparing
+																			// and
+																			// getting
+																			// the
+																			// result
+
+		// Printing hashCode
+		System.out.println(cyb.assignment1.test1.StringTest.name.hashCode());
+		System.out.println(name1.hashCode());
 
 	}
 
 }
 
-/*Output:
- * true
+
+/* Output: 
+ * true 
+ * -1732361581 
+ * -1732361581
  */
